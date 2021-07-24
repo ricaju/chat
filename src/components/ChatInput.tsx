295bab4f0801a7d8611241f11208cd40e9f54dd1
jurica <jurica.martinez@zeromolecule.com>
2021-07-24 14:10:13 +0200

    feat: add input component, first replay style

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -3,27 +3,28 @@ import style from "styles/components/ChatInput.module.scss"
 import { Button } from "./Button";
 import { ReactComponent as Plus } from "assets/icons/plus.svg"
 import { ReactComponent as Send } from "assets/icons/send.svg"
+import { Input } from "./Input";
 
 type Props = {}
 
 export const ChatInput: FC<Props> = memo(function ChatInput() {
-  const { handleUpload } = useChatInput();
+  const { handleUpload, handleSend } = useChatInput();
 
   return (
     <div className={style.root}>
       <Button onClick={handleUpload}><Plus/></Button>
-      <input/>
-      <Button onClick={handleUpload}>
+      <Input/>
+      <Button onClick={handleSend}>
         <Send/>
         <span className={style.buttonText}>Send message</span>
       </Button>
-
     </div>
   );
 });
 
 function useChatInput() {
   const handleUpload = useCallback(() => {}, [])
+  const handleSend = useCallback(() => {}, [])
 
-  return { handleUpload }
+  return { handleUpload, handleSend }
 }
